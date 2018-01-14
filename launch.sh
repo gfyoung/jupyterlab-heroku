3303f7b1654fe04a48f144cba5dfe5dcf0c222e4
gfyoung <gfyoung17@gmail.com>
2018-01-13 23:45:00 -0800

    Add Jupyter notebook config

--- launch.sh
+++ launch.sh
@@ -5,4 +5,5 @@ jupyter serverextension enable --py jupyterlab --sys-prefix
 mkdir -p root
 cd root
 
-jupyter lab --port=${PORT} --ip=* --no-browser
+export JUPYTER_CONFIG_DIR=/app
+jupyter lab --port=${PORT}
