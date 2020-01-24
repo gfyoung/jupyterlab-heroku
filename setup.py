3d09691b7ac1c12d66755ee2b7c52ad11efb3adc
Sami Djouadi <32199144+doubleShot666@users.noreply.github.com>
2020-01-24 17:33:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(name='kernel',
       version='0.1',
-      packages=['kernel'],
-      package_dir={'kernel': 'kernel'},
       description='kernelo lib',
-      package_data={'kernel': ['kernel.cpython-35m-x86_64-linux-gnu.so']}
+      py_modules=["kernel"],
+      packages=[''],
+      package_data={'': ['kernel.cpython-35m-x86_64-linux-gnu.so']},
 )
