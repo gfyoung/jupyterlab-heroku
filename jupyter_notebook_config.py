97b50263d938637142ed84a5a7d5ce26686647cd
Sami Djouadi <32199144+doubleShot666@users.noreply.github.com>
2020-01-24 15:57:08 +0100

    Update jupyter_notebook_config.py

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -4,3 +4,5 @@
 # we have SSL certificates enabled for encryption purposes.
 c.NotebookApp.ip = "*"
 c.NotebookApp.open_browser = False
+c.NotebookApp.password = u'sha1:a5c43a65be28:aeb69b90ca3d7395abf0c694d7ec680b3a2f29a7' #kernelo
+
