ad54513360d1564ac2c1e05642c65f04a9ed87bd
Sami Djouadi <32199144+doubleShot666@users.noreply.github.com>
2020-01-31 13:31:41 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,7 +5,7 @@ jupyter serverextension enable --py jupyterlab --sys-prefix
 mkdir -p root
 cd root
 
-cp ../kernel.cpython-36m-x86_64-linux-gnu.so ./kernel.cpython-36m-x86_64-linux-gnu.so
+cp ../kernel.cpython-37m-x86_64-linux-gnu.so ./kernel.cpython-37m-x86_64-linux-gnu.so
 
 export JUPYTER_CONFIG_DIR=/app
 jupyter lab --port=${PORT}
