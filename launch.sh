9434a67346e46464035459379f382968aacfdb20
Sami Djouadi <32199144+doubleShot666@users.noreply.github.com>
2020-02-03 10:23:58 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -6,6 +6,8 @@ mkdir -p root
 cd root
 
 cp ../kernel.cpython-37m-x86_64-linux-gnu.so ./kernel.cpython-37m-x86_64-linux-gnu.so
+cp ../test_hapke.json ./test_hapke.json
+cp ../test_script.ipynb ./test_script.ipynb
 
 
 export JUPYTER_CONFIG_DIR=/app
